e9a54e74a17be49d4730883f1d9f05b0e639ab68
Bek-art <beks.aliev2016@gmail.com>
2019-11-11 17:23:23 +0600

    adminCss

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
+import '../Admin/Admin.css'
+
 class Admin extends Component {
   render() {
     return (
       <div className="admin">
         <div className="container">
-          Admin
+          <table className="tableAdmin">
+          <tr>
+              <th>Date</th>
+              <th>Status</th> 
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+            </tr>
+            <tr>
+              <td>29.01.2019</td>
+              <td>accepted</td>
+              <td>Bek</td>
+              <td>0706201324</td>
+              <td>Ak-Orgo</td>
+            </tr> 
+            <tr>
+              <td>22.01.2019</td>
+              <td>not accepted</td>
+              <td>Samat</td>
+              <td>0706201323</td>
+              <td>Pishpek</td>
+            </tr>
+            <tr>
+              <td>21.01.2019</td>
+              <td>Done</td>
+              <td>Emir</td>
+              <td>0706201322</td>
+              <td>Alamedin</td>
+            </tr>
+            
+              
+          </table>
         </div>
       </div>
     );
