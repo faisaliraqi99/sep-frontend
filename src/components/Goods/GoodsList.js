2b76a45218810ed7dbb9e17023fe1ea71a51a919
Faisal Iraqi <faisal.iraqi99@gmail.com>
2019-11-12 14:46:23 +0600

    Conflict resolved

--- src/components/Goods/GoodsList.js
+++ src/components/Goods/GoodsList.js
@@ -11,21 +11,6 @@ class GoodsList extends Component {
     
     return (
       <>
-<<<<<<< HEAD
-        {goods.map(item => {
-          console.log(item)
-          return (
-          <div className="good" style={{backgroundImage: `url(http://localhost:8000/img/${item.img})`}} onClick={() => this.handleClick(item._id)} key={item._id}>
-            <div className="item-img-darkness"></div>
-            
-            <h3 className="item-goods">
-              {item.name}
-            </h3>
-            
-            <span className="item-price">
-              {item.price}
-            </span>
-=======
         {goods.map((item, index) => (
           <div className="good"  onClick={() => this.handleClick(item._id)} key={item._id}>
             <img alt={"item-img" + index} className="item-img" src={`http://localhost:8000/img/${item.img}`}></img>
@@ -38,7 +23,6 @@ class GoodsList extends Component {
                 {item.price}
               </span>
             </div>
->>>>>>> a25914775deb42652c16c8bbf7ee141668a94e77
           </div>
         )})}
       </>
