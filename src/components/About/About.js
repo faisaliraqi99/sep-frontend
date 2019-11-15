92b8f2f68b1110b335df6a41bf9550891acc6d2e
Bek-art <beks.aliev2016@gmail.com>
2019-11-16 00:24:06 +0600

    About CSS

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import jpg from '../About/img/s.jpg'
+import jpg from '../About/img/3807.jpg'
+
 import './About.css';
 
 class About extends React.Component{
@@ -17,9 +18,12 @@ class About extends React.Component{
               </div>
                   <div className="about-header-text">
 
-                    <h2>What is Lorem Ipsum?</h2>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
+                    <h2>Обычаи и традиции кыргызов</h2>
+                    Как и всех остальных народов мира, представляют собой сложный и богатый по своему содержанию этнический комплекс.  На формирование его характерных признаков большое влияние оказала тюрко-монгольская кочевая культура. Кроме того, в нем плотно переплелись обрядовые составляющие, возникшие в различные исторические эпохи. Поэтому, наряду с традициями ислама, здесь обнаруживается огромный пласт доисламских культов, обычаев и верований, которые зачастую играют главенствующую роль.
+                    <h1>lorem</h1>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+                    <h3>lorem</h3>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
                   </div>
             </div>
           </div>
